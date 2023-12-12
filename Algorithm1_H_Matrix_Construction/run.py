719207685cd0da0b93aab6caa684ab728a2382bf
Dongwhee Kim <62287136+xyz123479@users.noreply.github.com>
2023-12-12 17:20:47 +0900

    Update run.py

--- Algorithm1_H_Matrix_Construction/run.py
+++ Algorithm1_H_Matrix_Construction/run.py
@@ -2,6 +2,6 @@ import sys
 import os
 import time
 
-for i in range(30):
+for i in range(20):
     os.system("./nogada DFS_based_SSC_DEC_result_{0:02d}.txt {1:d} &".format(i,i))
     time.sleep(2) # rand seed 값 다르게 하기 위해 2초 간격으로 실행 시작하기
